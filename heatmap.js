7f3d8f141584c945b5b1d4e9c319862dc62bdbd8
Erin Black <erin.black@fieldnation.com>
2018-04-30 12:32:39 -0500

    added grid and sections to body of page and dropdowns

--- heatmap.js
+++ heatmap.js
@@ -5,22 +5,59 @@ $(function() {
         displayProviders(id);
         statesDropdown();
     }); // end industriesSelect
+
+    // Find the id of the selected state dropdown
+    $("#statesSelect").change(function() {
+        var id = $(this).children(":selected").attr("id");
+        cityDropdown(id);
+    });// end statesSelect
+
+    // Find the id of the selected state dropdown
+    $("#citiesSelect").change(function() {
+        addCity ();
+        var selectBox = document.getElementById("citiesSelect");
+        var selectedValue = selectBox.options[selectBox.selectedIndex ].value;
+            // Match selected city with matching img by countyFips value
+            function selectedImage(selectedValue){
+                for (var i = 0; i < map.dataProvider.images.length; i++) {
+                    var selectedImg = map.dataProvider.images[i];
+                    var countyFips = selectedImg.countyFips;
+                    if (countyFips == selectedValue){
+                        var selectedImgZoom =  selectedImg.zoomLevel;
+                        var selectedImgLat =  selectedImg.latitude;
+                        var selectedImgLong =  selectedImg.longitude;
+                        map.zoomToLongLat(selectedImgZoom, selectedImgLong, selectedImgLat, false);
+                        return;
+                    }
+                } //
+            } // end selectedImage
+            selectedImage(selectedValue);
+    }); // end citiesSelect
+
 });
 
 // svg path for target icon
 var targetSVG = "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z M9,15.93 c-3.83,0-6.93-3.1-6.93-6.93S5.17,2.07,9,2.07s6.93,3.1,6.93,6.93S12.83,15.93,9,15.93 M12.5,9c0,1.933-1.567,3.5-3.5,3.5S5.5,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z";
 
 AmCharts.ready(function() {
+    industriesDropdown();
     var select = document.getElementById( 'cities' );
     map = new AmCharts.AmMap();
 
-    map.colorSteps = 100;
+    map.colorSteps = 10;
 
     var dataProvider = {
         mapVar: AmCharts.maps.usCounties,
         images: [],
         areas: []
     }; // end dataProvider
+
+    map.zoomControl = {
+        buttonFillColor: '#5BC1A6',
+        buttonIconColor: '#ffffff',
+        homeButtonEnabled: true,
+        homeIconColor: '#ffffff'
+    };
 
     map.areasSettings = {
         autoZoom: true,
@@ -30,6 +67,10 @@ AmCharts.ready(function() {
         outlineAlpha: 0
     };
 
+    map.imagesSettings = {
+        color: '#707070'
+    };
+
     map.addClassNames = {
         addClassNames: true
     };
@@ -37,7 +78,7 @@ AmCharts.ready(function() {
     map.valueLegend = {
         right : 10,
         minValue : '<1',
-        maxValue : '50+',
+        maxValue : '250+',
 
     };
 
@@ -49,7 +90,6 @@ AmCharts.ready(function() {
        "zoomLatitude": map.zoomLatitude()
    };
 
-   addCity();
 }); //end AmCharts.ready
 
 
@@ -62,13 +102,12 @@ function addCity (){
           city.longitude = cities[x].lng;
           city.countyFips = cities[x].county_fips;
           city.svgPath = targetSVG;
-          city.zoomLevel = 5;
+          city.zoomLevel = 9;
           city.scale = 0.5;
           city.chart = map;
           map.dataProvider.images.push( city );
          city.validate();
      } // end for loop
-     industriesDropdown();
 } // end addCity
 
 
@@ -98,6 +137,24 @@ function statesDropdown(){
     select.appendChild( option );
   } // end for loop
 } // end stateDropdown
+
+// Populating Dropdown with all Cities
+function cityDropdown (stateId){
+    var select = document.getElementById( 'citiesSelect' );
+    $('#citiesSelect')
+        .find('option')
+        .remove()
+        .end()
+        .append('<option value="">- Select a City -</option>')
+    for ( var x in cities ) {
+        if(cities[x].state_id == stateId) {
+            var option = document.createElement( 'option' );
+            option.value = cities[ x ].county_fips;
+            option.text = cities[ x ].city + ', '+ cities[ x ].state_id ;
+            select.appendChild( option );
+        }
+  } // end for loop
+} // end cityDropdown
 
 // Display all Providers on the Counties map
 function displayProviders(id){
